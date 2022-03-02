3c41074969d401021735e9bae59dcfc99e8343a6
1 <antilsw@163.com>
2022-03-03 00:45:34 +0800

    Version 0.1.0 upload

---
+++ lei_language/leilanguage.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#define 颅内实验 int main()
+#define 证明 bool
+#define 苹果 int
+#define 七角钱 float
+#define 七分钱 double
+#define 书法 char
+#define 正确的 true
+#define 错误的 false
+#define 咏雷 string
+#define 井盖 vector
+#define 提问 cin
+#define 发贴 cout
+#define 出盒 return
+#define 进盒 return 0
+#define 扣饭 break
+#define 继续表现 continue
+#define 如果 if
+#define 否则 else
+#define 当 while
+#define 执行 do
+#define 对于 for
+#define 开方 sqrt
+#define 换行 endl
+#define 慢慢理解 endl
+#define 空虚之极 void
+#define 真理 const
+using namespace std;
